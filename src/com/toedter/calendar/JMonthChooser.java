26cdd7d49990302f7099539db16dc3132201b0cb
arg3ni5 <fb.argenis@gmail.com>
2015-06-07 18:28:52 -0600

    Mejoras y correccion de errores en UI

--- src/com/toedter/calendar/JMonthChooser.java
+++ src/com/toedter/calendar/JMonthChooser.java
@@ -22,382 +22,207 @@ package com.toedter.calendar;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.Locale;
-
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
-import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
  * JMonthChooser is a bean for choosing a month.
- * 
+ *
  * @author Kai Toedter
  * @version $LastChangedRevision: 159 $
  * @version $LastChangedDate: 2011-06-22 21:07:24 +0200 (Mi, 22 Jun 2011) $
  */
-public class JMonthChooser extends JPanel implements ItemListener,
-		ChangeListener {
-	private static final long serialVersionUID = -2028361332231218527L;
+public class JMonthChooser extends JPanel {
 
-	/** true, if the month chooser has a spinner component */
-	protected boolean hasSpinner;
+    private static final long serialVersionUID = -2028361332231218527L;
 
-	private Locale locale;
+    /**
+     * true, if the month chooser has a spinner component
+     */
+    protected boolean hasSpinner;
 
-	private int month;
+    private int month;
 
-	private int oldSpinnerValue = 0;
+    private int oldSpinnerValue = 0;
 
-	// needed for comparison
-	private JDayChooser dayChooser;
+    private JSpinnerMonth spinner;
 
-	private JYearChooser yearChooser;
+    private boolean initialized;
 
-	private JComboBox comboBox;
+    private boolean localInitialize;
+    private JComboBox comboBox;
 
-	private JSpinner spinner;
+    /**
+     * Default JMonthChooser constructor.
+     */
+    public JMonthChooser() {
+        this(true);
+    }
 
-	private boolean initialized;
+    /**
+     * JMonthChooser constructor with month spinner parameter.
+     *
+     * @param hasSpinner true, if the month chooser should have a spinner
+     * component
+     */
+    public JMonthChooser(boolean hasSpinner) {
+        super();
+        setName("JMonthChooser");
+        this.hasSpinner = hasSpinner;
 
-	private boolean localInitialize;
+        setLayout(new BorderLayout());
+        spinner = new JSpinnerMonth();
 
-	/**
-	 * Default JMonthChooser constructor.
-	 */
-	public JMonthChooser() {
-		this(true);
-	}
+        if (hasSpinner) {
+            add(spinner.getSpinner(), BorderLayout.WEST);
+        } else {
+            comboBox = (JComboBox) spinner.getComboBox();
+            add(comboBox, BorderLayout.WEST);
+        }
 
-	/**
-	 * JMonthChooser constructor with month spinner parameter.
-	 * 
-	 * @param hasSpinner
-	 *            true, if the month chooser should have a spinner component
-	 */
-	public JMonthChooser(boolean hasSpinner) {
-		super();
-		setName("JMonthChooser");
-		this.hasSpinner = hasSpinner;
+        initialized = true;
+        setMonth(Calendar.getInstance().get(Calendar.MONTH));
+    }
 
-		setLayout(new BorderLayout());
+    /**
+     * Sets the month. This is a bound property. Valuse are valid between 0
+     * (January) and 11 (December). A value < 0 will be treated as 0, a value >
+     * 11 will be treated as 11.
+     *
+     * @param newMonth the new month value
+     *
+     * @see #getMonth
+     */
+    public void setMonth(int newMonth) {
+        spinner.setMonth(newMonth);
+    }
 
-		comboBox = new JComboBox();
-		comboBox.addItemListener(this);
+    /**
+     * Returns the month.
+     *
+     * @return the month value
+     */
+    public int getMonth() {
+        return spinner.getMonth();
+    }
 
-		// comboBox.addPopupMenuListener(this);
-		locale = Locale.getDefault();
-		initNames();
+    /**
+     * Convenience method set a day chooser.
+     *
+     * @param dayChooser the day chooser
+     */
+    public void setDayChooser(JDayChooser dayChooser) {
+        this.spinner.setDayChooser(dayChooser);
+    }
 
-		if (hasSpinner) {
-			spinner = new JSpinner() {
-				private static final long serialVersionUID = 1L;
+    /**
+     * Convenience method set a year chooser. If set, the spin for the month
+     * buttons will spin the year as well
+     *
+     * @param yearChooser the new yearChooser value
+     */
+    public void setYearChooser(JYearChooser yearChooser) {
+        this.spinner.setYearChooser(yearChooser);
+    }
 
-				private JTextField textField = new JTextField();
+    /**
+     * Returns the locale.
+     *
+     * @return the locale value
+     *
+     * @see #setLocale
+     */
+    public Locale getLocale() {
+        return spinner.getLocale();
+    }
 
-				public Dimension getPreferredSize() {
-					Dimension size = super.getPreferredSize();
-					return new Dimension(size.width, textField
-							.getPreferredSize().height);
-				}
-			};
-			spinner.addChangeListener(this);
-			spinner.setEditor(comboBox);
-			comboBox.setBorder(new EmptyBorder(0, 0, 0, 0));
-			updateUI();
+    /**
+     * Set the locale and initializes the new month names.
+     *
+     * @param l the new locale value
+     *
+     * @see #getLocale
+     */
+    public void setLocale(Locale l) {
+        if (!initialized) {
+            spinner.setLocale(l);
+        }
+    }
 
-			add(spinner, BorderLayout.WEST);
-		} else {
-			add(comboBox, BorderLayout.WEST);
-		}
+    /**
+     * Enable or disable the JMonthChooser.
+     *
+     * @param enabled the new enabled value
+     */
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
 
-		initialized = true;
-		setMonth(Calendar.getInstance().get(Calendar.MONTH));
-	}
+        if (spinner != null) {
+            spinner.setEnabled(enabled);
+        }
+    }
 
-	/**
-	 * Initializes the locale specific month names.
-	 */
-	public void initNames() {
-		localInitialize = true;
+    /**
+     * Returns the month chooser's comboBox text area (which allow the focus to
+     * be set to it).
+     *
+     * @return the combo box
+     */
+    public Component getComboBox() {
+        return this.spinner.getComboBox();
+    }
 
-		DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(locale);
-		String[] monthNames = dateFormatSymbols.getMonths();
+    /**
+     * Returns the month chooser's comboBox bar (which allow the focus to be set
+     * to it).
+     *
+     * @return Component the spinner or null, if the month chooser has no
+     * spinner
+     */
+    public Component getSpinner() {
+        // Returns <null> if there is no spinner.
+        return spinner.getSpinner();
+    }
 
-		if (comboBox.getItemCount() == 12) {
-			comboBox.removeAllItems();
-		}
-
-		for (int i = 0; i < 12; i++) {
-			comboBox.addItem(monthNames[i]);
-		}
-
-		localInitialize = false;
-		comboBox.setSelectedIndex(month);
-	}
-
-	/**
-	 * Is invoked if the state of the spinner changes.
-	 * 
-	 * @param e
-	 *            the change event.
-	 */
-	public void stateChanged(ChangeEvent e) {
-		SpinnerNumberModel model = (SpinnerNumberModel) ((JSpinner) e
-				.getSource()).getModel();
-		int value = model.getNumber().intValue();
-		boolean increase = (value > oldSpinnerValue) ? true : false;
-		oldSpinnerValue = value;
-
-		int month = getMonth();
-
-		if (increase) {
-			month += 1;
-
-			if (month == 12) {
-				month = 0;
-
-				if (yearChooser != null) {
-					int year = yearChooser.getYear();
-					year += 1;
-					yearChooser.setYear(year);
-				}
-			}
-		} else {
-			month -= 1;
-
-			if (month == -1) {
-				month = 11;
-
-				if (yearChooser != null) {
-					int year = yearChooser.getYear();
-					year -= 1;
-					yearChooser.setYear(year);
-				}
-			}
-		}
-
-		setMonth(month);
-	}
-
-	/**
-	 * The ItemListener for the months.
-	 * 
-	 * @param e
-	 *            the item event
-	 */
-	public void itemStateChanged(ItemEvent e) {
-		if (e.getStateChange() == ItemEvent.SELECTED) {
-			int index = comboBox.getSelectedIndex();
-
-			if ((index >= 0) && (index != month)) {
-				setMonth(index, false);
-			}
-		}
-	}
-
-	/**
-	 * Sets the month attribute of the JMonthChooser object. Fires a property
-	 * change "month".
-	 * 
-	 * @param newMonth
-	 *            the new month value
-	 * @param select
-	 *            true, if the month should be selcted in the combo box.
-	 */
-	private void setMonth(int newMonth, boolean select) {
-		if (!initialized || localInitialize) {
-			return;
-		}
-
-		int oldMonth = month;
-		month = newMonth;
-
-		if (select) {
-			comboBox.setSelectedIndex(month);
-		}
-
-		if (dayChooser != null) {
-			dayChooser.setMonth(month);
-		}
-
-		firePropertyChange("month", oldMonth, month);
-	}
-
-	/**
-	 * Sets the month. This is a bound property. Valuse are valid between 0
-	 * (January) and 11 (December). A value < 0 will be treated as 0, a value >
-	 * 11 will be treated as 11.
-	 * 
-	 * @param newMonth
-	 *            the new month value
-	 * 
-	 * @see #getMonth
-	 */
-	public void setMonth(int newMonth) {
-		if (newMonth < 0 || newMonth == Integer.MIN_VALUE) {
-			setMonth(0, true);
-		} else if (newMonth > 11) {
-			setMonth(11, true);
-		} else {
-			setMonth(newMonth, true);
-		}
-	}
-
-	/**
-	 * Returns the month.
-	 * 
-	 * @return the month value
-	 */
-	public int getMonth() {
-		return month;
-	}
-
-	/**
-	 * Convenience method set a day chooser.
-	 * 
-	 * @param dayChooser
-	 *            the day chooser
-	 */
-	public void setDayChooser(JDayChooser dayChooser) {
-		this.dayChooser = dayChooser;
-	}
-
-	/**
-	 * Convenience method set a year chooser. If set, the spin for the month
-	 * buttons will spin the year as well
-	 * 
-	 * @param yearChooser
-	 *            the new yearChooser value
-	 */
-	public void setYearChooser(JYearChooser yearChooser) {
-		this.yearChooser = yearChooser;
-	}
-
-	/**
-	 * Returns the locale.
-	 * 
-	 * @return the locale value
-	 * 
-	 * @see #setLocale
-	 */
-	public Locale getLocale() {
-		return locale;
-	}
-
-	/**
-	 * Set the locale and initializes the new month names.
-	 * 
-	 * @param l
-	 *            the new locale value
-	 * 
-	 * @see #getLocale
-	 */
-	public void setLocale(Locale l) {
-		if (!initialized) {
-			super.setLocale(l);
-		} else {
-			locale = l;
-			initNames();
-		}
-	}
-
-	/**
-	 * Enable or disable the JMonthChooser.
-	 * 
-	 * @param enabled
-	 *            the new enabled value
-	 */
-	public void setEnabled(boolean enabled) {
-		super.setEnabled(enabled);
-		comboBox.setEnabled(enabled);
-
-		if (spinner != null) {
-			spinner.setEnabled(enabled);
-		}
-	}
-
-	/**
-	 * Returns the month chooser's comboBox text area (which allow the focus to
-	 * be set to it).
-	 * 
-	 * @return the combo box
-	 */
-	public Component getComboBox() {
-		return this.comboBox;
-	}
-
-	/**
-	 * Returns the month chooser's comboBox bar (which allow the focus to be set
-	 * to it).
-	 * 
-	 * @return Component the spinner or null, if the month chooser has no
-	 *         spinner
-	 */
-	public Component getSpinner() {
-		// Returns <null> if there is no spinner.
-		return spinner;
-	}
-
-	/**
-	 * Returns the type of spinner the month chooser is using.
-	 * 
-	 * @return true, if the month chooser has a spinner
-	 */
-	public boolean hasSpinner() {
-		return hasSpinner;
-	}
+    /**
+     * Returns the type of spinner the month chooser is using.
+     *
+     * @return true, if the month chooser has a spinner
+     */
+    public boolean hasSpinner() {
+        return hasSpinner;
+    }
 
     /**
      * Sets the font for this component.
      *
      * @param font the desired <code>Font</code> for this component
      */
-	public void setFont(Font font) {
-		if (comboBox != null) {
-			comboBox.setFont(font);
-		}
-		super.setFont(font);
-	}
+    public void setFont(Font font) {
+        if (spinner != null) {
+            spinner.setFont(font);
+        }
+    }
 
-	/**
-	 * Updates the UI.
-	 * 
-	 * @see javax.swing.JPanel#updateUI()
-	 */
-	public void updateUI() {
-		final JSpinner testSpinner = new JSpinner();
-		if (spinner != null) {
-			if ("Windows".equals(UIManager.getLookAndFeel().getID())) {
-				spinner.setBorder(testSpinner.getBorder());
-			} else {
-				spinner.setBorder(new EmptyBorder(0, 0, 0, 0));
-			}
-		}
-	}
-
-	/**
-	 * Creates a JFrame with a JMonthChooser inside and can be used for testing.
-	 * 
-	 * @param s
-	 *            The command line arguments
-	 */
-	public static void main(String[] s) {
-		JFrame frame = new JFrame("MonthChooser");
-		frame.getContentPane().add(new JMonthChooser());
-		frame.pack();
-		frame.setVisible(true);
-	}
+    /**
+     * Creates a JFrame with a JMonthChooser inside and can be used for testing.
+     *
+     * @param s The command line arguments
+     */
+    public static void main(String[] s) {
+        JFrame frame = new JFrame("MonthChooser");
+        frame.getContentPane().add(new JMonthChooser());
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
